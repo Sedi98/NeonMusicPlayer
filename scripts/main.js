476a33f5b70c9e7d5d71c26412ff8532b6aae134
Sedi98 <sedi9816@gmail.com>
2024-07-07 16:37:51 +0400

    First Commit

---
+++ scripts/main.js
@@ -0,0 +1,181 @@
+function loadMusicList() {
+  musicList.innerHTML = "";
+
+  musicsData.forEach((data, index) => {
+    musicList.innerHTML += `
+    <div class="listItem" data-index="${index}" onclick="playMusic(${index})">
+      <img class="miniCover" src="${data.cover}" alt="" />
+      <div>
+      <p class="listMusicName">${data.name}</p>
+      <p class="listMusicArtist">${data.artist}</p>
+      </div>
+      <div class="equalizerIcon">
+      <i class='bx bxs-bar-chart-alt-2 bx-flashing' ></i>
+      </div>
+    </div>
+    `;
+  });
+}
+
+function playMusic(index) {
+  currentMusic.pause();
+  currentMusic.currentTime = 0;
+
+  currentMusic = new Audio(musicsData[index].audioLocation);
+  currentMusic.play();
+  loadPlayer(index);
+  playerControls(index);
+  activeMusicIndex = index;
+
+  timeInterval = setInterval(updateProgress, 1000);
+}
+
+function loadPlayer(musicIndex) {
+  musicMarker(musicIndex);
+  musicPlayer.innerHTML = `
+    <img src="${musicsData[musicIndex].cover}" alt="" class="coverImage" />
+    <div class="playerInfo">
+      <h3 class="musicNameText">${musicsData[musicIndex].name}</h3>
+      <p class="artistNameText">${musicsData[musicIndex].artist}</p>
+    </div>
+    <div class="progressBar">
+      <div class="progress"></div>
+    </div>
+    <div class="timeSection">
+       <p class="currentTimeText">--:-- </p>
+       <p class="durationText">--:-- </p>
+    </div>
+    
+    <div class="PlayerButtons">
+     <div class="shuffleBtn "><i class='bx bx-shuffle' ></i></div>
+      <div class="prevTrackBtn"><i class='bx bx-skip-previous' ></i></div>
+      <div class="playBtn" > <i class='bx bx-pause'></i></div>
+      <div class="nxtTrackBtn"><i class='bx bx-skip-next' ></i></div>
+      <div class="repeatBtn"><i class='bx bx-repeat'></i></div>
+    </div>
+    `;
+
+  shuffleBtn = document.querySelector(".shuffleBtn");
+  repeatBtn = document.querySelector(".repeatBtn");
+
+  if (isShuffling) {
+    shuffleBtn.classList.add("activeBtn");
+  } else {
+    shuffleBtn.classList.remove("activeBtn");
+  }
+
+  if (isRepeating) {
+    repeatBtn.classList.add("activeBtn");
+  } else {
+    repeatBtn.classList.remove("activeBtn");
+  }
+
+  shuffleBtn.addEventListener("click", () => {
+    shuffleBtn.classList.toggle("activeBtn");
+    isShuffling = !isShuffling;
+    console.log(isShuffling);
+  });
+  repeatBtn.addEventListener("click", () => {
+    repeatBtn.classList.toggle("activeBtn");
+    isRepeating = !isRepeating;
+    console.log(isRepeating);
+  });
+}
+
+function updateProgress() {
+  let progress = document.querySelector(".progress");
+  let currentTime = document.querySelector(".currentTimeText");
+  let duration = document.querySelector(".durationText");
+
+  progress.style.width = `${
+    (currentMusic.currentTime / currentMusic.duration) * 100
+  }%`;
+
+  currentTime.innerHTML = timeConvert(currentMusic.currentTime);
+  duration.innerHTML = timeConvert(currentMusic.duration);
+
+  if (currentMusic.ended) {
+    clearInterval(timeInterval);
+    nextTrack(activeMusicIndex);
+  }
+}
+
+function timeConvert(time) {
+  let min = Math.floor(time / 60);
+  if (min < 10) {
+    min = "0" + min;
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  return min + ":" + sec;
+}
+
+function playerControls(index) {
+  let prevTrackBtn = document.querySelector(".prevTrackBtn");
+  let playBtn = document.querySelector(".playBtn");
+  let nxtTrackBtn = document.querySelector(".nxtTrackBtn");
+  playBtn.addEventListener("click", () => {
+    if (currentMusic.paused) {
+      currentMusic.play();
+      playBtn.innerHTML = `<i class='bx bx-pause'></i>`;
+    } else {
+      currentMusic.pause();
+      playBtn.innerHTML = `<i class='bx bx-play'></i>`;
+    }
+  });
+
+  prevTrackBtn.addEventListener("click", () => {
+    prevTrack(index);
+  });
+  nxtTrackBtn.addEventListener("click", () => {
+    nextTrack(index);
+  });
+}
+
+function nextTrack(index) {
+  if (isShuffling) {
+    let randomIndex = Math.floor(Math.random() * musicsData.length);
+    playMusic(randomIndex);
+  } else {
+    if (isRepeating) {
+      playMusic(index);
+    } else {
+      if (index == musicsData.length - 1) {
+        playMusic(0);
+      } else {
+        playMusic(index + 1);
+      }
+    }
+  }
+
+  //   if (index == musicsData.length - 1) {
+  //     playMusic(0);
+  //   } else {
+  //     playMusic(index + 1);
+  //   }
+}
+
+function prevTrack(index) {
+  if (index == 0) {
+    playMusic(musicsData.length - 1);
+  } else {
+    playMusic(index - 1);
+  }
+}
+
+function musicMarker(index) {
+  let listItems = document.querySelectorAll(".listItem");
+  let equalizerIcon = document.querySelectorAll(".equalizerIcon");
+
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].classList.remove("activelistItem");
+    equalizerIcon[i].style.display="none";
+  }
+
+  listItems[index].classList.add("activelistItem");
+  equalizerIcon[index].style.display="block";
+}
+
+loadMusicList();
---
+++ scripts/variables.js
@@ -0,0 +1,110 @@
+const musicsData = [
+  {
+    name: "Lay All Your Love On Me",
+    artist: "ABBA",
+    audioLocation: "../assets/musics/ABBA-Lay All Your Love On Me(Official Lyric Video).mp3",
+    cover: "../assets/covers/layallyourlove.jpg",
+  },
+  {
+    name: "Hymn for the Weekend",
+    artist: "Coldplay",
+    audioLocation: "../assets/musics/Alan Walker vs Coldplay-Hymn For The Weekend.mp3",
+    cover: "../assets/covers/hymnof.jpg",
+  },
+  {
+    name: "Angetenar",
+    artist: "Rompasso",
+    audioLocation: "../assets/musics/Angetenar (Original Mix).mp3",
+    cover: "../assets/covers/angetenar.jpg",
+  },
+  {
+    name: "Bloody Mary (Remix)",
+    artist: "Lady Gaga",
+    audioLocation:
+      "../assets/musics/Bloody Mary - Best Part Instrumental (Slowed  Reverb).mp3",
+    cover: "../assets/covers/bloodymary.jpg",
+  },
+  {
+    name: "Crystals",
+    artist: "Isolate.exe & Andrej Simon",
+    audioLocation:
+      "../assets/musics/Crystals.mp3",
+    cover: "../assets/covers/crystals.jpg",
+  },
+  {
+    name: "Happy Nation",
+    artist: "Ace of Base",
+    audioLocation:
+      "../assets/musics/Happy Nation - Ace of Base (Slowed  Reverb).mp3",
+    cover: "../assets/covers/happynation.jpg",
+  },
+  {
+    name: "In the end",
+    artist: "Linkin Park & Mellen Gi",
+    audioLocation:
+      "../assets/musics/Linkin Park - In The End (Mellen Gi & Tommee Profitt Remix).mp3",
+    cover: "../assets/covers/intheend.jpeg",
+  },
+  {
+    name: "Little Dark Age",
+    artist: "MGMT",
+    audioLocation: "../assets/musics/MGMT - Little Dark Age.mp3",
+    cover: "../assets/covers/littledarkage.jpg",
+  },
+  {
+    name: "Dark Beach",
+    artist: "Pastel Ghost",
+    audioLocation:
+      "../assets/musics/Pastel Ghost - Dark beach (Tiktok Version).mp3",
+    cover: "../assets/covers/darkbeach.jpg",
+  },
+  {
+    name: "The Lost Soul Down X Lost Soul",
+    artist: "NBSPLV",
+    audioLocation:
+      "../assets/musics/The Lost Soul Down X Lost Soul.mp3",
+    cover: "../assets/covers/lostsoul.jpg",
+  },
+
+
+  {
+    name: "Night Drive",
+    artist: "Wilee",
+    audioLocation:
+      "../assets/musics/Wilee-Night Drive.mp3",
+    cover: "../assets/covers/nightdrive.jpg",
+  },
+];
+
+let musicList = document.querySelector(".musicList");
+let musicPlayer = document.querySelector(".musicPlayer");
+let coverImage = document.querySelector(".coverImage");
+let musicNameText = document.querySelector(".musicNameText");
+let artistNameText = document.querySelector(".artistNameText");
+let progressBar = document.querySelector(".progressBar");
+
+
+let listItems;
+
+
+let equalizerIcon;
+
+let shuffleBtn;
+
+let repeatBtn;
+
+let currentMusic = new Audio();
+
+
+let timeInterval;
+
+
+let activeMusicIndex = 0;
+
+
+let isRepeating = false;
+
+let isShuffling = false;
+
+
+let htmlTitle = document.querySelector("htmlTitle");
